9e54f919281897266da9fb7536c150e44415dc48
Kevin Qi <kqi@patientslikeme.com>
2016-04-26 14:07:36 -0400

    Add shiftDate helper

---
+++ src/shiftDate.js
@@ -0,0 +1,6 @@
+// returns a new date shifted a certain number of days (can be negative)
+export default function shiftDate(date, numDays) {
+  const newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + numDays);
+  return newDate;
+}
