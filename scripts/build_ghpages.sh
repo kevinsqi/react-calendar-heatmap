bdc1b319c2f5815fa85038799ebf7ba9816c7d29
Kevin Qi <iqnivek@gmail.com>
2016-05-16 14:07:57 -0400

    Update ghpages script

--- scripts/build_ghpages.sh
+++ scripts/build_ghpages.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
 cp -R ./demo .cache
+
+rm .cache/demo.jsx
+rm .cache/styles.css
 cp src/styles.css .cache
+
 git checkout gh-pages
+
 mv ./.cache/* .
-rm demo.jsx
 rm -rf .cache
+
 git add .
 git commit -m "Update demo page"
 git push
