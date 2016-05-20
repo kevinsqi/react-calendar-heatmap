1afe1fdfe291a6acfdee2eb28d70ff74ab3a5ad3
Kevin Qi <iqnivek@gmail.com>
2016-05-20 15:52:44 -0400

    Reorganize and add classForValue test

--- test/components/CalendarHeatmap.spec.js
+++ test/components/CalendarHeatmap.spec.js
@@ -16,32 +16,36 @@ describe('CalendarHeatmap', () => {
   it('should not throw exceptions in base case', () => {
     assert.doesNotThrow(() => <CalendarHeatmap values={[]} />);
   });
+});
 
-  it('should be able to orient horizontally and vertically', () => {
+describe('CalendarHeatmap props', () => {
+  it('horizontal', () => {
     const horizontal = shallow(
       <CalendarHeatmap numDays={100} values={[]} horizontal />
     );
-    const [, , horWidth, horHeight] = horizontal.props().viewBox.split(' ');
+    const [, , horWidth, horHeight] = horizontal.prop('viewBox').split(' ');
     assert(Number(horWidth) > Number(horHeight), 'horizontal orientation width should be greater than height');
 
     const vertical = shallow(
       <CalendarHeatmap numDays={100} values={[]} horizontal={false} />
     );
-    const [, , vertWidth, vertHeight] = vertical.props().viewBox.split(' ');
+    const [, , vertWidth, vertHeight] = vertical.prop('viewBox').split(' ');
     assert(Number(vertWidth) < Number(vertHeight), 'vertical orientation width should be less than height');
   });
 
-  it('should end on the correct day', () => {
+  it('endDate', () => {
     const today = new Date();
     const wrapper = shallow(
       <CalendarHeatmap values={[]} endDate={today} numDays={10} />
     );
 
-    assert.equal(today.getDate(), wrapper.instance().getEndDate().getDate());
-    assert.equal(today.getMonth(), wrapper.instance().getEndDate().getMonth());
+    assert(
+      today.getDate() === wrapper.instance().getEndDate().getDate() &&
+      today.getMonth() === wrapper.instance().getEndDate().getMonth()
+    );
   });
 
-  it('should start on the correct day', () => {
+  it('numDays', () => {
     const today = new Date();
     const numDays = 10;
     const expectedStartDate = shiftDate(today, -numDays + 1);
@@ -49,7 +53,41 @@ describe('CalendarHeatmap', () => {
       <CalendarHeatmap values={[]} endDate={today} numDays={numDays} />
     );
 
-    assert.equal(expectedStartDate.getDate(), wrapper.instance().getStartDate().getDate());
-    assert.equal(expectedStartDate.getMonth(), wrapper.instance().getStartDate().getMonth());
+    assert(
+      expectedStartDate.getDate() === wrapper.instance().getStartDate().getDate() &&
+      expectedStartDate.getMonth() === wrapper.instance().getStartDate().getMonth()
+    );
+
+    assert.equal(numDays, wrapper.find('rect').length);
+  });
+
+  it('classForValue', () => {
+    const today = new Date();
+    const numDays = 10;
+    const expectedStartDate = shiftDate(today, -numDays + 1);
+    const wrapper = shallow(
+      <CalendarHeatmap
+        values={[
+          { date: expectedStartDate, count: 0 },
+          { date: today, count: 1 },
+        ]}
+        endDate={today}
+        numDays={numDays}
+        titleForValue={(value) => (value ? value.count : null)}
+        classForValue={(value) => {
+          if (!value) {
+            return null;
+          }
+          return value.count > 0 ? 'red' : 'white';
+        }}
+      />
+    );
+
+    assert.equal(1, wrapper.find('.white').length);
+    assert.equal(1, wrapper.find('.red').length);
+
+    // TODO these attr selectors might be broken with react 15
+    // assert(wrapper.first('rect[title=0]').hasClass('white'));
+    // assert(wrapper.first('rect[title=1]').hasClass('red'));
   });
 });
