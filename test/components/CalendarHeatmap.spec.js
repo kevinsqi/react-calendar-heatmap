7f0430933b971464111d3b158cbdef821c3315b4
Kevin Qi <iqnivek@gmail.com>
2016-05-20 14:26:04 -0400

    Add horizontal/vertical orientation test
    Update mocha setup.js

--- test/components/CalendarHeatmap.spec.js
+++ test/components/CalendarHeatmap.spec.js
@@ -5,12 +5,28 @@ import CalendarHeatmap from '../../src';
 
 describe('CalendarHeatmap', () => {
   it('should render as an svg', () => {
-    const wrapper = shallow(<CalendarHeatmap values={[]} />);
+    const wrapper = shallow(
+      <CalendarHeatmap values={[]} />
+    );
     assert.equal(1, wrapper.find('svg').length);
   });
 
   it('should not throw exceptions in base case', () => {
     assert.doesNotThrow(() => <CalendarHeatmap values={[]} />);
+  });
+
+  it('should be able to orient horizontally and vertically', () => {
+    const horSVG = shallow(
+      <CalendarHeatmap numDays={100} values={[]} horizontal={true} />
+    ).find('svg');
+    const [, , horWidth, horHeight] = horSVG.props().viewBox.split(' ');
+    assert(Number(horWidth) > Number(horHeight), 'horizontal orientation width should be greater than height');
+
+    const vertSVG = shallow(
+      <CalendarHeatmap numDays={100} values={[]} horizontal={false} />
+    ).find('svg');
+    const [, , vertWidth, vertHeight] = vertSVG.props().viewBox.split(' ');
+    assert(Number(vertWidth) < Number(vertHeight), 'vertical orientation width should be less than height');
   });
 
   it.skip('should start on the correct day', () => {
--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,18 @@
-import { jsdom } from 'jsdom';
+// https://github.com/airbnb/enzyme/blob/master/docs/guides/jsdom.md#using-enzyme-with-jsdom
 
-global.document = jsdom('<!doctype html><html><body></body></html>');
+var jsdom = require('jsdom').jsdom;
+
+var exposedProperties = ['window', 'navigator', 'document'];
+
+global.document = jsdom('');
 global.window = document.defaultView;
-global.navigator = global.window.navigator;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    exposedProperties.push(property);
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
