20a985f0586c1463a4c7bc10a0b998350701b284
Dragan Filipovic <dragan.filipovic@netcentric.biz>
2017-08-12 00:15:46 +0200

    #34 [update] Use PropTypes from separate package

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import range from 'lodash.range';
 import reduce from 'lodash.reduce';
 import { DAYS_IN_WEEK, MILLISECONDS_IN_ONE_DAY, MONTH_LABELS } from './constants';
