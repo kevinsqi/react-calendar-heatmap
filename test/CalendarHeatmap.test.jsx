7ecb6d4a157658e3d54d127e84f3926bf652ab45
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2017-10-29 12:56:25 -0400

    upgrade to react 16 for local dev

--- test/CalendarHeatmap.test.jsx
+++ test/CalendarHeatmap.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 
 import CalendarHeatmap from '../src';
 import { dateNDaysAgo, shiftDate } from '../src/helpers';
