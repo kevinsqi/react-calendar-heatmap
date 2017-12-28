e0118e5698f835e2ac28063b71578e7863ab6ff3
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2017-12-28 11:29:23 -0500

    fix lint errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,21 @@
 const webpack = require('webpack');
 const path = require('path');
 const pak = require('./package.json');
+
 const nodeEnv = process.env.NODE_ENV || 'development';
 
 const webpackConfig = {
   context: __dirname,
   entry: {
     'react-calendar-heatmap': [
-      path.resolve(__dirname, 'src', 'index.jsx')
-    ]
+      path.resolve(__dirname, 'src', 'index.jsx'),
+    ],
   },
   output: {
     path: path.resolve(__dirname),
     filename: 'index.js',
     library: 'CalendarHeatmap',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -26,21 +27,21 @@ const webpackConfig = {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
         use: [{
-          loader: 'babel-loader'
-        }]
-      }
-    ]
+          loader: 'babel-loader',
+        }],
+      },
+    ],
   },
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(nodeEnv),
     }),
-  ]
+  ],
 };
 
 if (nodeEnv === 'development') {
   webpackConfig.devtool = 'source-map';
-  webpackConfig.devServer = { contentBase: './demo'};
+  webpackConfig.devServer = { contentBase: './demo' };
   webpackConfig.entry['react-calendar-heatmap'].unshift('webpack-dev-server/client?http://0.0.0.0:8080/');
   webpackConfig.entry['react-calendar-heatmap'].push(path.resolve(__dirname, 'demo', 'demo.jsx'));
   webpackConfig.output.publicPath = '/';
@@ -53,25 +54,25 @@ if (nodeEnv === 'demo') {
 
 if (nodeEnv === 'development' || nodeEnv === 'demo') {
   webpackConfig.plugins.push(new webpack.DefinePlugin({
-    'COMPONENT_NAME': JSON.stringify(pak.name),
-    'COMPONENT_VERSION': JSON.stringify(pak.version),
-    'COMPONENT_DESCRIPTION': JSON.stringify(pak.description)
+    COMPONENT_NAME: JSON.stringify(pak.name),
+    COMPONENT_VERSION: JSON.stringify(pak.version),
+    COMPONENT_DESCRIPTION: JSON.stringify(pak.description),
   }));
 }
 
 if (nodeEnv === 'production') {
   webpackConfig.externals = {
-    'react': {
+    react: {
       root: 'React',
       commonjs2: 'react',
       commonjs: 'react',
-      amd: 'react'
-    }
+      amd: 'react',
+    },
   };
   webpackConfig.output.path = path.resolve(__dirname, 'build');
   webpackConfig.plugins.push(new webpack.optimize.UglifyJsPlugin({
     compress: { warnings: false },
-    sourceMap: false
+    sourceMap: false,
   }));
 }
 
