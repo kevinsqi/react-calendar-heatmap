208b50a9229196071598a5ff94843b0bd1c568da
Kevin Qi <kqi@amino.com>
2018-08-25 16:12:41 -0700

    rewrite webpack config, add prettier, add deps to demo

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,26 @@
 const webpack = require('webpack');
 const path = require('path');
-const pak = require('./package.json');
 
-const nodeEnv = process.env.NODE_ENV || 'development';
+const isProduction = process.env.NODE_ENV === 'production';
+const plugins = [];
 
-const webpackConfig = {
+if (isProduction) {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      sourceMap: false,
+    }),
+  );
+}
+
+module.exports = {
   context: __dirname,
   entry: {
-    'react-calendar-heatmap': [
-      path.resolve(__dirname, 'src', 'index.jsx'),
-    ],
+    'react-calendar-heatmap': [path.resolve(__dirname, 'src', 'index.jsx')],
   },
   output: {
-    path: path.resolve(__dirname),
-    filename: 'index.js',
+    path: path.resolve(__dirname, 'build'),
+    filename: isProduction ? 'index.min.js' : 'index.js',
     library: 'CalendarHeatmap',
     libraryTarget: 'umd',
   },
@@ -26,54 +33,21 @@ const webpackConfig = {
       {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
-        use: [{
-          loader: 'babel-loader',
-        }],
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
       },
     ],
   },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(nodeEnv),
-    }),
-  ],
-};
-
-if (nodeEnv === 'development') {
-  webpackConfig.devtool = 'source-map';
-  webpackConfig.devServer = { contentBase: './demo' };
-  webpackConfig.entry['react-calendar-heatmap'].unshift('webpack-dev-server/client?http://0.0.0.0:8080/');
-  webpackConfig.entry['react-calendar-heatmap'].push(path.resolve(__dirname, 'demo', 'demo.jsx'));
-  webpackConfig.output.publicPath = '/';
-}
-
-if (nodeEnv === 'demo') {
-  webpackConfig.entry['react-calendar-heatmap'].push(path.resolve(__dirname, 'demo', 'demo.jsx'));
-  webpackConfig.output.path = path.resolve(__dirname, 'demo');
-}
-
-if (nodeEnv === 'development' || nodeEnv === 'demo') {
-  webpackConfig.plugins.push(new webpack.DefinePlugin({
-    COMPONENT_NAME: JSON.stringify(pak.name),
-    COMPONENT_VERSION: JSON.stringify(pak.version),
-    COMPONENT_DESCRIPTION: JSON.stringify(pak.description),
-  }));
-}
-
-if (nodeEnv === 'production') {
-  webpackConfig.externals = {
+  externals: {
     react: {
       root: 'React',
       commonjs2: 'react',
       commonjs: 'react',
       amd: 'react',
     },
-  };
-  webpackConfig.output.path = path.resolve(__dirname, 'build');
-  webpackConfig.plugins.push(new webpack.optimize.UglifyJsPlugin({
-    compress: { warnings: false },
-    sourceMap: false,
-  }));
-}
-
-module.exports = webpackConfig;
+  },
+  plugins,
+};
